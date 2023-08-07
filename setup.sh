eee18200a14f8bb888701d286b783a482fbf5f0c
Michael Leue <domus@posteo.de>
2023-08-07 07:08:12 +0200

    simplify setup script

--- setup.sh
+++ setup.sh
@@ -1,35 +1,15 @@
 #!/bin/bash
-# sets up the dotfiles and environments for vim, tmux and bash
-
 # setting up variables
-dotfiles_dir=~/.dotfiles/             	# dotfiles directory
-backup_dotfiles_dir=~/.dotfiles_backup/ # dotfiles backup directory
-backup_config_dir=~/.config_backup/ 	# config dirs backup directory
-dotfiles=".vimrc .tmux.conf .bashrc .gdbinit .bash_profile"  	# list of files to symlink
-configdirs="sway waybar nvim"  			# list of .config dirs to symlink
+dotfiles_dir=~/.dotfiles/
+dotfiles_dir_backup=~/.dotfiles.bak/
+mkdir -p $dotfiles_dir_backup
+# list of dotfiles/dirs to symlink
+dotfiles=".vimrc .tmux.conf .bashrc .gdbinit .bash_profile .config/sway .config/waybar .config/nvim .xkb"
 
-# create backup dotfiles dir in homedir
-echo "Creating $backup_dotfiles_dir for backing up any existing dotfiles in ~"
-mkdir -p $backup_dotfiles_dir
-echo "...done"
-
-# change to the dotfiles directory
-echo "Changing to the $dotfiles_dir directory"
-cd $dotfiles_dir
-echo "...done"
-
-# for any dotfile, first back it up, then create a new symlink in ~ to the dotfile
+# for any dotfile, first back it up, then create a new symlink in ~/ to it
 for file in $dotfiles; do
-	echo "Backing up the existing $file into $backup_dotfiles_dir."
-    mv ~/$file $backup_dotfiles_dir
+	echo "Backing up existing $file into $dotfiles_dir_backup"
+    mv ~/$file $dotfiles_dir_backup
     echo "Creating symlink to $file in home directory."
-    ln -sf -v $dotfiles_dir$file ~/$file
-done
-
-# for any .config dir, first back it up, then symlink to it
-for dir in $configdirs; do
-	echo "Backing up the existing $dir into $backup_config_dir."
-    mv ~/.config/$dir $backup_dotfiles_dir
-    echo "Creating symlink to $dir in home .config directory."
-    ln -snf -v $dotfiles_dir.config/$dir ~/.config/$dir
+    ln -snf -v $dotfiles_dir$file ~/$file
 done
